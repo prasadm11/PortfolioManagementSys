c7f183ff63afab81ad98faa530b3ea58017848ea
PrasadmNeosoft <prasad.mahajan@neosoftmail.com>
2025-08-31 14:29:46 +0530

    Update Program.cs
    yes

--- PortfolioManamagement.API/Program.cs
+++ PortfolioManamagement.API/Program.cs
@@ -112,15 +112,15 @@ namespace PortfolioManamagement.API
       var app = builder.Build();
 
       // Configure the HTTP request pipeline.
-     if (app.Environment.IsDevelopment())
-      {
+     //if (app.Environment.IsDevelopment())
+     // {
         app.UseSwagger();
         app.UseSwaggerUI(c =>
         {
           c.SwaggerEndpoint("/swagger/v1/swagger.json", "Portfolio Management API v1");
           c.RoutePrefix = string.Empty; 
         });
-      }
+      //}
 
       app.UseHttpsRedirection();
 
